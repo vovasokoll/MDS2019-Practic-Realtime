d558df29e8df499d7e4b5cd3f44338955d4fc630
Sokolovich Vladimir <bily-777@yandex.ru>
2019-01-30 22:42:07 +0300

    appcenter: first commit

--- app/src/main/java/com/example/mds2018/MainActivity.java
+++ app/src/main/java/com/example/mds2018/MainActivity.java
@@ -2,11 +2,6 @@ package com.example.mds2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.ndk.CrashlyticsNdk;
-import io.fabric.sdk.android.Fabric;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
         setContentView(R.layout.activity_main);
 
         final TextView version = findViewById(R.id.version);
