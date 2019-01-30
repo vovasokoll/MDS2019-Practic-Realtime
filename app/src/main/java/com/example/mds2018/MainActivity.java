6d08d69265c7bdf7858941eac7e695c810a483ff
Sokolovich Vladimir <bily-777@yandex.ru>
2019-01-30 23:08:34 +0300

    appcenter: key version 101

--- app/src/main/java/com/example/mds2018/MainActivity.java
+++ app/src/main/java/com/example/mds2018/MainActivity.java
@@ -4,7 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+
+
+import com.microsoft.appcenter.distribute.Distribute;
+import com.microsoft.appcenter.AppCenter;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String APP_CENTER_KEY = "f42aded6-68b0-4af0-8c44-bd83d467758a";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView version = findViewById(R.id.version);
         version.setText(BuildConfig.VERSION_NAME);
+
+        AppCenter.start(getApplication(), APP_CENTER_KEY, Distribute.class);
     }
 }
