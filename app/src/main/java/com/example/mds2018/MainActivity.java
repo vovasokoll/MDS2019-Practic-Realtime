36a94bc812c942622d95953e7afccfe417182e3d
Sokolovich Vladimir <bily-777@yandex.ru>
2019-01-30 22:21:45 +0300

    feature: fabric and congif deploy

--- app/src/main/java/com/example/mds2018/MainActivity.java
+++ app/src/main/java/com/example/mds2018/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.mds2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.ndk.CrashlyticsNdk;
+import io.fabric.sdk.android.Fabric;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
         setContentView(R.layout.activity_main);
+
+        final TextView version = findViewById(R.id.version);
+        version.setText(BuildConfig.VERSION_NAME);
     }
 }
